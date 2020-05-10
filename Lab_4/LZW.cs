2dacb6102250d41fb7148c3501f247f62fbe77cd
Nagnogur <trostinetsky.sasha1@gmail.com>
2020-05-10 18:06:22 +0300

    Added LZW class

---
+++ Lab_4/LZW.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class LZW
+    {
+        Dictionary<string, byte> table = new Dictionary<string, byte>();
+        public void InitialTable()
+        {
+            for (int i = 0; i < 256; i++)
+            {
+                table.Add(Convert.ToString((char)i), (byte)i);
+            }
+        }
+
+    }
+}
--- Lab_4/Program.cs
+++ Lab_4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,13 @@ namespace Lab_4
     {
         static void Main(string[] args)
         {
-
+            byte[] s = File.ReadAllBytes(@"1.txt");
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i]);
+            }
+            
+                Console.ReadKey();
         }
     }
 }
