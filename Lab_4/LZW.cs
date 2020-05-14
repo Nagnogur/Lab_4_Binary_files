22027e2735decceebace756e424855cfdde41458
Nagnogur <trostinetsky.sasha1@gmail.com>
2020-05-14 16:47:40 +0300

    Integers represents as 2 bytes

--- Lab_4/LZW.cs
+++ Lab_4/LZW.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,41 +8,8 @@ namespace Lab_4
 {
     class LZW
     {
-        List <string> files { set; get; }
-        string name { get; set; }
-        public LZW(List<string> input, string name)
-        {
-            files = input;
-            this.name = name;
-        }
-        public LZW(string name)
-        {
-            this.name = name;
-        }
-        public void Compress()
-        {
-            List<byte> bytes = new List<byte>();
-            bytes.Add(Convert.ToByte(files.Count));
-            foreach (var file in files)
-            {
-                byte[] fileContent = File.ReadAllBytes(@file);
-                byte[] fileName = Encoding.ASCII.GetBytes(file);
-                bytes.Add(Convert.ToByte(fileContent.Length));
-                bytes.Add(Convert.ToByte(fileName.Length));
-                bytes.AddRange(fileContent.Concat(fileName).ToArray());
-            }
-            byte[] input = bytes.ToArray();
-            int[] output = Compress1(input);
-            using (BinaryWriter writer = new BinaryWriter(File.Open(@name, FileMode.Create)))
-            {
-                for (int i = 0; i < output.Length; i++)
-                {
-                    writer.Write(output[i]);
-                }
-            }
-        }
 
-        private int[] Compress1(byte[] input)
+        public int[] Compress(byte[] input)
         {
             Dictionary<string, int> table = new Dictionary<string, int>();
             for (int i = 0; i < 256; i++)
@@ -74,72 +40,22 @@ namespace Lab_4
                     table.Add(s + c, k);
                     s = c;
                     k++;
-                    
+
                 }
             }
             res.Add(table[s]);
-            
+
             return res.ToArray();
         }
 
-        public void Decompress()
-        {
-            List<int> input = Array.ConvertAll(File.ReadAllBytes(@name), c => (int)c).ToList();
-            int fileNumber = input[0];
-            input.RemoveAt(0);
-            int[] content = new int[2 * fileNumber];
-            int n = 0;
-            for (int i = 0; i < fileNumber; i++)
-            {
-                content[2 * i] = input[n];
-                content[2 * i + 1] = input[n + 1];
-                input.RemoveRange(n, 2);
-                n += content[2 * i] + content[2 * i + 1];
-            }
-            int[] com = input.ToArray();
-            string[] dec = Decompress1(com);
-            
-            List<byte> decompress = new List<byte>();
-            for (int i = 0; i < dec.Length; i++)
-            {
-                for (int j = 0; j < dec[i].Length; j++)
-                {
-                    decompress.Add((byte)dec[i][j]);
-                }
-            }
-            string[] names = new string[fileNumber];
-            n = 0;
-            for (int i = 0; i < fileNumber; i++)
-            {
-                n += content[2 * i];
-                for (int j = n; j < content[2 * i + 1]; j++)
-                {
-
-                }
-            }
-
-            for (int i = 0; i < fileNumber; i++)
-            {
-                int j = 0;
-                using (BinaryWriter writer = new BinaryWriter(File.Open(@"11.mp3", FileMode.Create)))
-                {
-                    for (int i = 0; i < decompress.Count; i++)
-                    {
-                        writer.Write(decompress[i]);
-                    }
-                }
-            }
-
-        }
-
-        private string[] Decompress1(int[] input)
+        public string[] Decompress(int[] input)
         {
             Dictionary<int, string> table = new Dictionary<int, string>();
             for (int i = 0; i < 256; i++)
             {
                 table.Add(i, Convert.ToString((char)i));
             }
-            
+
             int code = input[0];
             int k = 256,
                 len = input.Length,
--- Lab_4/Program.cs
+++ Lab_4/Program.cs
@@ -11,57 +11,36 @@ namespace Lab_4
     {
         static void Main(string[] args)
         {
-            /*switch(args[0])
-            {
-                case "compress":
-                    {
-                        List<string> files = new List<string>();
-                        for (int i = 2; i < args.Length; i++)
-                        {
-                            files.Add(args[i]);
-                        }
-                        LZW archive = new LZW(files);
-                        
-                        archive.Compress();
-                        break;
-                    }
-            }*/
 
-            if (Console.ReadLine() == "compress")
-            {
-                int n = Convert.ToInt32(Console.ReadLine());
-                string output = Console.ReadLine();
-                List<string> files = new List<string>();
-                for (int i = 0; i < n; i++)
-                {
-                    files.Add(Console.ReadLine());
-                }
-                LZW archive = new LZW(files, output);
-                archive.Compress();
-            }
-            else
-            {
-                string file = Console.ReadLine();
-                LZW archive = new LZW(file);
-                archive.Decompress();
-            }
-
-            byte[] s = File.ReadAllBytes(@"story.mp3");
-            LZW test = new LZW(files, output);
+            byte[] s = File.ReadAllBytes(@"mouse.bmp");
+            LZW test = new LZW();
             int[] b = test.Compress(s);
+            byte[] bytes = new byte[b.Length * 2];
             for (int i = 0; i < b.Length; i++)
             {
-                Console.Write(b[i] + " ");
+                bytes[i * 2] = (byte)(b[i] & 0xFF);
+                bytes[i * 2 + 1] = (byte)((b[i] >> 8) & 0xFF);
             }
-            using (BinaryWriter writer = new BinaryWriter(File.Open(@"1.LZW", FileMode.Create)))
+            using (BinaryWriter writer = new BinaryWriter(File.Open(@"mouse.LZW", FileMode.Create)))
             {
-                for (int i = 0; i < b.Length; i++)
+                for (int i = 0; i < bytes.Length; i++)
                 {
-                    writer.Write(b[i]);
+                    writer.Write(bytes[i]);
                 }
             }
 
-            string[] dec = test.Decompress(b);
+            byte[] s1 = File.ReadAllBytes(@"mouse.LZW");
+            int[] bb = new int[s1.Length / 2];
+            for (int i = 0; i < bb.Length; i++)
+            {
+                bb[i] = s1[i * 2] | s1[i * 2 + 1] << 8;
+            }
+
+            for (int i = 0; i < bb.Length; i++)
+            {
+                Console.Write(bb[i] + " ");
+            }
+            string[] dec = test.Decompress(bb);
             List<byte> decompress = new List<byte>();
             for (int i = 0; i < dec.Length; i++)
             {
@@ -70,7 +49,7 @@ namespace Lab_4
                     decompress.Add((byte)dec[i][j]);
                 }
             }
-            using (BinaryWriter writer = new BinaryWriter(File.Open(@"11.mp3", FileMode.Create)))
+            using (BinaryWriter writer = new BinaryWriter(File.Open(@"mouse_dec.bmp", FileMode.Create)))
             {
                 for (int i = 0; i < decompress.Count; i++)
                 {
